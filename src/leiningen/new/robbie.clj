4e8bd87ddfa68742f406778b813d6cd86a1449ed
Chris Bilson <cbilson@pobox.com>
2012-03-30 07:10:26 -0700

    add support for frameworks (jquery-mobile is it for now)

--- src/leiningen/new/robbie.clj
+++ src/leiningen/new/robbie.clj
@@ -3,7 +3,7 @@
         [leiningen.new.templates :only [renderer render-text sanitize slurp-resource]]
         [clojure.java.io :as io]))
 
-(defn android-create [{:keys [target name package activity]}]
+(defn- android-create [{:keys [target name package activity]}]
   (println "Creating android project targeting" target)
   (let [{:keys [exit err]}
         (sh "android" "create" "project"
@@ -17,31 +17,64 @@
 
 (def render (renderer "robbie"))
 
-(defn copy-files [{:keys [name] :as data} & paths]
+(defn- copy-files [{:keys [name] :as data} paths]
   (doseq [[src-path dest-path] paths]
     (println "copying" src-path)
     (let [content (slurp-resource (io/file "leiningen" "new" "robbie" src-path))
-          dest (io/file name dest-path)]
+          dest (io/file name (if (nil? dest-path) src-path dest-path))]
       (.mkdirs (.getParentFile dest))
       (spit dest content))))
 
-(defn copy-binary-files [{:keys [name] :as data} & paths]
+(defn- copy-binary-files [{:keys [name] :as data} paths]
   (doseq [[src-path dest-path] paths]
     (println "copying" src-path)
-    (let [dest (io/file name dest-path)]
+    (let [dest (io/file name (if (nil? dest-path) src-path dest-path))]
       (.mkdirs (.getParentFile dest))
       (with-open [input (-> (io/file "leiningen" "new" "robbie" src-path) .getPath io/resource io/input-stream)]
         (with-open [output (io/output-stream dest)]
           (copy input output))))))
 
-(defn generate-files
-  [{:keys [name] :as data} & items]
+(defn- generate-files [{:keys [name] :as data} items]
   (doseq [item items]
-    (let [[relative-path content] item
-          path (io/file name relative-path)]
-      (println "Generating" relative-path)
-      (.mkdirs (.getParentFile path))
-      (spit path content))))
+    (let [[template dest-path] item
+          dest (io/file name (if (nil? dest-path) template dest-path))]
+      (println "Generating" template)
+      (.mkdirs (.getParentFile dest))
+      (spit dest (render template data)))))
+
+(defmulti framework-specific-files (comp keyword :framework))
+
+(defmethod framework-specific-files :none [project-data] {})
+
+(defmethod framework-specific-files :jquery-mobile-1.0 [generic-project-data]
+  {:stylesheets [{:href "jquery.mobile-1.0.1.min.css"}]
+   :scripts [{:src "jquery-1.7.1.min.js"}
+             {:src "jquery.mobile-1.0.1.min.js"}]
+   :text-files [["jquery.mobile-1.0.1.min.css" "assets/www/jquery.mobile-1.0.1.min.css"]
+                ["jquery-1.7.1.min.js" "assets/www/jquery-1.7.1.min.js"]
+                ["jquery.mobile-1.0.1.min.js" "assets/www/jquery.mobile-1.0.1.min.js"]]})
+
+(defmethod framework-specific-files :jquery-mobile-1.1 [generic-project-data]
+  {:stylesheets [{:href "jquery.mobile-1.1.0-rc.1.min.css"}]
+   :scripts [{:src "jquery-1.7.1.min.js"}
+             {:src "jquery.mobile-1.1.0-rc.1.min.js"}]
+   :text-files [["jquery.mobile-1.1.0-rc.1.min.css" "assets/www/jquery.mobile-1.1.0-rc.1.min.css"]
+                ["jquery-1.7.1.min.js" "assets/www/jquery-1.7.1.min.js"]
+                ["jquery.mobile-1.1.0-rc.1.min.js" "assets/www/jquery.mobile-1.1.0-rc.1.min.js"]]})
+
+(defn- concat-in [m k xs]
+  (assoc m k (concat xs (k m))))
+
+(defn- merge-in [left-project-data right-project-data]
+  (letfn [($concat [lhs k] (concat-in lhs k (k right-project-data)))]
+    (reduce $concat left-project-data [:scripts :stylesheets :template-files :text-files :binary-files])))
+
+(defn- generate-project [{:keys [text-files binary-files template-files] :as project-data}]
+  (clojure.pprint/pprint project-data)
+  (android-create project-data)
+  (copy-files project-data text-files)
+  (copy-binary-files project-data binary-files)
+  (generate-files project-data template-files))
 
 (defn- options->keyword-map [options]
   (let [pairs (partition 2 options)
@@ -50,35 +83,44 @@
     (zipmap keys vals)))
 
 (def default-options
-  {:target "android-7"})
+  {:target "android-7"
+   :framework :none
+   :stylesheets [{:href "master.css"}]
+   :scripts [{:src "phonegap-1.4.1.js"}]
+   :text-files [["phonegap-1.4.1.js" "assets/www/phonegap-1.4.1.js"]
+                ["master.css" "assets/www/master.css"]
+                ["gitignore" ".gitignore"]]
+   :binary-files [["phonegap-1.4.1.jar" "libs/phonegap-1.4.1.jar"]
+                  ["icon.png" "res/drawable/icon.png"]]
+   :template-files [["README.md"]
+                    ["project.clj"]
+                    ["AndroidManifest.xml"]
+                    ["phonegap.xml" "res/xml/phonegap.xml"]
+                    ["plugins.xml" "res/xml/plugins.xml"]
+                    ["strings.xml" "res/values/strings.xml"]
+                    ["index.html" "assets/www/index.html"]]})
 
-(defn ^:no-project-needed robbie
-  "An Android Phonegap ClojureScript project template"
+(defn ^{:no-project-needed true}
+  robbie
+  "An Android Phonegap ClojureScript project template
+
+ Give the name for your new project, followed by the following options:
+   target     the android version you want to target. [default: android-7]
+              Use `android list targets` to see what your options are.
+   framework  include a javascript mobile framework [default: none]
+              Options include: jquery-mobile-1.0, jquery-mobile-1.1"
   [name & specified-options]
-  (let [{:keys [target]} (merge default-options (options->keyword-map specified-options))
-        sanitized-name (sanitize name)
+  (let [sanitized-name (sanitize name)
         package-path (str "src/com/" sanitized-name)
         activity (str (clojure.string/capitalize sanitized-name) "Activity")
-        data {:name sanitized-name
-              :original-name name
-              :target target
-              :package (str "com." sanitized-name)
-              :activity activity}]
-    (android-create data)
-    (copy-files data
-                ["phonegap-1.4.1.js" "assets/www/phonegap-1.4.1.js"]
-                ["master.css" "assets/www/master.css"]
-                ["gitignore" ".gitignore"])
-    (copy-binary-files data
-                       ["phonegap-1.4.1.jar" "libs/phonegap-1.4.1.jar"]
-                       ["icon.png" "res/drawable/icon.png"])
-    (generate-files data
-                    ["README.md" (render "README.md" data)]
-                    ["project.clj" (render "project.clj" data)]
-                    ["AndroidManifest.xml" (render "AndroidManifest.xml" data)]
-                    ["res/xml/phonegap.xml" (render "phonegap.xml" data)]
-                    ["res/xml/plugins.xml" (render "plugins.xml" data)]
-                    ["res/values/strings.xml" (render "strings.xml" data)]
-                    ["assets/www/index.html" (render "index.html" data)]
-                    [(str package-path "/" activity ".java") (render "Activity.java" data)]
-                    [(str "src-cljs/" name "/core.cljs") (render "core.cljs" data)])))
+        project-data (-> default-options
+                         (merge (options->keyword-map specified-options))
+                         (merge {:name sanitized-name
+                                 :original-name name
+                                 :package (str "com." sanitized-name)
+                                 :activity activity}))]
+    (-> project-data
+        (merge-in (framework-specific-files project-data))
+        (merge-in {:template-files [["Activity.java" (str package-path "/" activity ".java")]
+                                    ["core.cljs" (str "src-cljs/" name "/core.cljs")]]})
+        (generate-project))))
