6e8e21988984a40fc0e023eaebc76d5498ed58d1
Chris Bilson <cbilson@pobox.com>
2012-03-30 07:09:39 -0700

    fix the cljsbuild parts of project.clj template

--- src/leiningen/new/robbie/project.clj
+++ src/leiningen/new/robbie/project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :cljsbuild {:source-path "src-cljs"
-              :compiler
-              {:output-to "assets/www/{{name}}.js"
-               :externs "resources/externs/phonegap-externs.js"
-               :optimizations :simple
-               :pretty-print true}}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :plugins [[lein-cljsbuild "0.1.3"]]
+  :cljsbuild {:builds
+              [{:source-path "src-cljs"
+                :compiler
+                {:output-to "assets/www/{{name}}.js"
+                 :externs "resources/externs/phonegap-externs.js"
+                 :optimizations :simple
+                 :pretty-print true}}]})
