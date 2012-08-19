9510cf9403860b9a1d2c9b91b19777f5deb4d237
Chris Bilson <cbilson@pobox.com>
2012-08-19 11:48:43 -0700

    exclude .DS_Store files from inclusion in the JAR

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+  :jar-exclusions [#"\.DS_Store"]
   :deps [[lein-newnew "0.3.4"]
          [cljsbuild "0.2.5"]])
